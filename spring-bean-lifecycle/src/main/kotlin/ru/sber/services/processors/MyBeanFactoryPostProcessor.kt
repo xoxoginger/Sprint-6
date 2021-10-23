07ded178063d996d60d5a78fb7bcfd9e349b8639
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-10-23 17:34:43 +0300

    upd bean lifecycle

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
@@ -2,10 +2,19 @@ package ru.sber.services.processors
 
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+import org.springframework.core.annotation.AnnotationUtils
 import org.springframework.stereotype.Component
+import ru.sber.services.BeanFactoryPostProcessorBean
+import javax.annotation.PostConstruct
 
 @Component
 class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor {
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
+        val methods = BeanFactoryPostProcessorBean::class.java.methods
+
+        for (method in methods) {
+            if (AnnotationUtils.findAnnotation(method, PostConstruct::class.java) != null)
+                beanFactory.getBeanDefinition("beanFactoryPostProcessorBean").initMethodName = method.name
+        }
     }
 }
