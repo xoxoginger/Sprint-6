ccc5736a79dc9d1b576e2fa31c690be2169a3b1e
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-10-23 17:08:08 +0300

    upd bean lifecycle

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -8,6 +8,6 @@ import ru.sber.services.CombinedBean
 @Configuration
 @ComponentScan("ru.sber.services")
 class ServicesConfig {
-    @Bean
+    @Bean(initMethod = "customInit")
     fun combinedBean() = CombinedBean()
 }
