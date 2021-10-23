50eaab0ed2795cb69fa36c15ebcdf2697b1724da
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-10-23 17:37:37 +0300

    upd bean lifecycle

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanPostProcessor.kt
@@ -2,14 +2,19 @@ package ru.sber.services.processors
 
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.stereotype.Component
+import ru.sber.services.CombinedBean
 
 @Component
 class MyBeanPostProcessor : BeanPostProcessor {
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
+        if (bean is CombinedBean)
+            bean.postProcessBeforeInitializationOrderMessage = "postProcessBeforeInitialization() is called"
         return bean
     }
 
     override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
+        if (bean is CombinedBean)
+            bean.postProcessAfterInitializationOrderMessage = "postProcessAfterInitialization() is called"
         return bean
     }
 }
