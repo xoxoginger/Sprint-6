dec157f99ffe2b4a91759f7502f4fb077eb900b9
Juli <62210049+xoxoginger@users.noreply.github.com>
2021-10-23 16:53:09 +0300

    upd java config

--- spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -4,20 +4,28 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import ru.sber.services.FirstService
+import ru.sber.services.SecondService
+import ru.sber.services.ThirdService
 
 @Configuration
 @ComponentScan
 class ServicesConfig {
     @Bean
-    fun service(): FirstService {
+    fun firstService(): FirstService {
         return FirstService()
     }
 
     @Bean
-    fun secondService() {
+    fun secondService(): SecondService {
+        return SecondService()
+    }
+
+    @Bean
+    fun thirdService(): ThirdService {
+        return ThirdService()
     }
 }
 
 @Configuration
-@ComponentScan("ru.sber.anotherservices")
+@ComponentScan("ru.sber.services")
 class AnotherServicesConfig
